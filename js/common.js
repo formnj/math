18d3c7119c06b1eace8ef7680c87f54b97cc84f8
jonghwan <subdiary@daum.net>
2023-08-28 15:46:19 +0900

    teacher main

--- js/common.js
+++ js/common.js
@@ -48,24 +48,3 @@ function toggle_slide(_target) {
         )
     )
 }
-
-/* progress bar */
-function progress_bar(_target){
-    var _target = $(_target),
-        _bar_W = Number(_target.find('em').text());
-        _target.find('.progress_bar').css('width', _bar_W+'%');
-        
-    var text_pos = setInterval(function(){
-        if (_target.children('.progress_bar').outerWidth() < (_target.find('em').outerWidth() + 1)) {
-            _target.find('em').css({
-                marginRight:-_target.find('em').outerWidth()
-            });
-        } else {
-            _target.find('em').removeAttr('style');
-        }
-
-        setTimeout(function(){
-            clearInterval(text_pos);
-        },1500); // 프로그래스 바 애니메이션이 1초 동안 진행되므로 여유시간 포함 1.5초가 지난 후엔 텍스트 위치 검사 중지
-    },10); // 프로그래스 바 넓이가 변하는 동안 0.01초에 한번씩 검사해서 텍스트의 위치를 변경해주기 위함
-}

c6f7fbfb6d1c51f10a670200a859ef2bb71f5745
jonghwan <subdiary@daum.net>
2023-08-30 17:37:38 +0900

    css & tablet 진단하기

--- js/window.js
+++ js/window.js
@@ -3,35 +3,12 @@ $(function(){
     var user_info = $('.user_info').outerHeight();
     var noti = $('section.noti').outerHeight() + 16;
 
+    $('section.diagnosis').css('maxHeight', module_right - user_info - noti -16);
+
+
     var diagnosisOuterHeight = $('section.diagnosis').outerHeight()
     var diagnosis_Header = $('section.diagnosis h2').outerHeight();
     var diagnosis_Title = $('section.diagnosis ul').outerHeight();
 
-    var boundary = $('section.diagnosis').offset().top + $('section.diagnosis').height();
-
-    if(boundary > module_right){
-        $('section.diagnosis').css('height', module_right - user_info - noti -16);
-        $('section.diagnosis dl').css('maxHeight', diagnosisOuterHeight - diagnosis_Header - diagnosis_Title - 16);
-    } else {
-        $('section.diagnosis, section.diagnosis dl').removeAttr('style');
-    }
-
-    $(window).resize(function(){
-        var module_right = $('.module_right').outerHeight() - 30;
-        var user_info = $('.user_info').outerHeight();
-        var noti = $('section.noti').outerHeight() + 16;
-
-        var diagnosisOuterHeight = $('section.diagnosis').outerHeight()
-        var diagnosis_Header = $('section.diagnosis h2').outerHeight();
-        var diagnosis_Title = $('section.diagnosis ul').outerHeight();
-
-        var boundary = $('section.diagnosis').offset().top + $('section.diagnosis').height();
-
-        // if(boundary > module_right){
-            $('section.diagnosis').css('height', module_right - user_info - noti -16);
-            $('section.diagnosis dl').css('maxHeight', diagnosisOuterHeight - diagnosis_Header - diagnosis_Title - 16);
-        // } else {
-        //     $('section.diagnosis, section.diagnosis dl').removeAttr('style');
-        // }
-    });
+    $('section.diagnosis dl').css('maxHeight', diagnosisOuterHeight - diagnosis_Header - diagnosis_Title - 16);
 })

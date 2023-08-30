29649d993466f25c4dc1fe3f325ca6c3edeef072
jonghwan <subdiary@daum.net>
2023-08-30 15:25:04 +0900

    css

--- js/window.js
+++ js/window.js
@@ -1,23 +1,37 @@
 $(function(){
-  $(window).resize(function(){
     var module_right = $('.module_right').outerHeight() - 30;
     var user_info = $('.user_info').outerHeight();
     var noti = $('section.noti').outerHeight() + 16;
-    $('section.diagnosis').css('height', module_right - user_info - noti + 32);
-        
-    // var offsetTop = $('section.diagnosis').offset().top;
+
     var diagnosisOuterHeight = $('section.diagnosis').outerHeight()
-    var diagnosis_Header = $('.diagnosis h2').outerHeight();
-    var diagnosis_Title = $('.diagnosis ul').outerHeight();
-    $('.diagnosis dl').css('maxHeight', diagnosisOuterHeight - diagnosis_Header - diagnosis_Title + 32)
-    })//resize
+    var diagnosis_Header = $('section.diagnosis h2').outerHeight();
+    var diagnosis_Title = $('section.diagnosis ul').outerHeight();
+
+    var boundary = $('section.diagnosis').offset().top + $('section.diagnosis').height();
+
+    if(boundary > module_right){
+        $('section.diagnosis').css('height', module_right - user_info - noti -16);
+        $('section.diagnosis dl').css('maxHeight', diagnosisOuterHeight - diagnosis_Header - diagnosis_Title - 16);
+    } else {
+        $('section.diagnosis, section.diagnosis dl').removeAttr('style');
+    }
+
+    $(window).resize(function(){
+        var module_right = $('.module_right').outerHeight() - 30;
+        var user_info = $('.user_info').outerHeight();
+        var noti = $('section.noti').outerHeight() + 16;
+
+        var diagnosisOuterHeight = $('section.diagnosis').outerHeight()
+        var diagnosis_Header = $('section.diagnosis h2').outerHeight();
+        var diagnosis_Title = $('section.diagnosis ul').outerHeight();
+
+        var boundary = $('section.diagnosis').offset().top + $('section.diagnosis').height();
+
+        // if(boundary > module_right){
+            $('section.diagnosis').css('height', module_right - user_info - noti -16);
+            $('section.diagnosis dl').css('maxHeight', diagnosisOuterHeight - diagnosis_Header - diagnosis_Title - 16);
+        // } else {
+        //     $('section.diagnosis, section.diagnosis dl').removeAttr('style');
+        // }
+    });
 })
-
-
-// window.addEventListener('resize', function(){
-//   var offsetTop = document.querySelector('section.diagnosis').offsetTop;
-//   var diagnosis_Header = document.querySelector('.diagnosis h2').offsetHeight;
-//   var diagnosis_Title = document.querySelector('.diagnosis ul').offsetHeight;
-//   document.querySelector('.diagnosis dl').style.maxHeight = (offsetTop - diagnosis_Header - diagnosis_Title - 16) + 'px'
-
-// })/
